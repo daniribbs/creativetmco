4047e53cc617483e855a583fddeea8bfbd0679b8
daniribbs <dani@creativetm.co>
2025-10-02 21:12:01 -0300

    Update and rename general.js to menu.js

--- menu.js
+++ menu.js
@@ -1,13 +1,4 @@
 document.addEventListener("DOMContentLoaded", function() {
-  document.querySelectorAll('.portfolio-list-image').forEach(track => {
-    const clone = track.innerHTML;
-    for (let i = 0; i < 4; i++) { // repete 3 vezes
-      track.innerHTML += clone;
-    }
-  });
-});
-
-document.addEventListener("DOMContentLoaded", function() {
   const menus = document.querySelectorAll('.service-item-menu');
   const details = document.querySelectorAll('.filtered-item');
 
