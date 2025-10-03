4527e0ac6e3b87281ec9999b499fe5a0d070000f
daniribbs <dani@creativetm.co>
2025-10-03 16:34:49 -0300

    Update clients-logos-marquee.js

--- clients-logos-marquee.js
+++ clients-logos-marquee.js
@@ -1,8 +1,1 @@
-document.addEventListener("DOMContentLoaded", function() {
-  document.querySelectorAll('.logo-lista').forEach(track => {
-    const clone = track.innerHTML;
-    for (let i = 0; i < 3; i++) { // repete 3 vezes
-      track.innerHTML += clone;
-    }
-  });
-});
+
