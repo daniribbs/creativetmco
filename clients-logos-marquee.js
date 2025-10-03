6131e86656dfc3134e9a71c738cb752c795133f2
daniribbs <dani@creativetm.co>
2025-10-03 16:42:10 -0300

    Update clients-logos-marquee.js

--- clients-logos-marquee.js
+++ clients-logos-marquee.js
@@ -1,1 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+  document.querySelectorAll('.logo-lista').forEach(track => {
+    const clone = track.innerHTML;
+    for (let i = 0; i < 4; i++) { // repete 3 vezes
+      track.innerHTML += clone;
+    }
+  });
+});
 
