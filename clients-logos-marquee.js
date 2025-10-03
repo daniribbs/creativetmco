a08ec1e15745fa17cd2a97f634c1062e4712bb78
daniribbs <dani@creativetm.co>
2025-10-03 16:19:43 -0300

    Create clients-logos-marquee.js

---
+++ clients-logos-marquee.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+  document.querySelectorAll('.logo-lista').forEach(track => {
+    const clone = track.innerHTML;
+    for (let i = 0; i < 8; i++) { // repete 3 vezes
+      track.innerHTML += clone;
+    }
+  });
+});
