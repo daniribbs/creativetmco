909da43751888b0d5ebd5cd3dfc4bafd8a29a773
daniribbs <dani@creativetm.co>
2025-12-23 12:47:00 -0300

    Update portfolio-marquee.js

--- portfolio-marquee.js
+++ portfolio-marquee.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
   document.querySelectorAll('.portfolio-list-image').forEach(track => {
     const clone = track.innerHTML;
-    for (let i = 0; i < 3; i++) { // repete 3 vezes
+    for (let i = 0; i < 2; i++) { // repete 3 vezes
       track.innerHTML += clone;
     }
   });
